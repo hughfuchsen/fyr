53a0ad7cd400c07786aca9c89bfda4757c6cd78c
hughfuchsen <hughfuchsen@gmail.com>
2025-03-26 13:26:37 +1100

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -5,9 +5,8 @@ import ArtistGrid from "./ArtistGrid";
 export default function LandingPage() {
   const [expandedArtist, setExpandedArtist] = useState(null);
   const [backgroundImage, setBackgroundImage] = useState("/background_image_fyr.jpg");
-  const [addMarginTop, setAddMarginTop] = useState(false);
-
   const backgroundRef = useRef(null);
+  const lineRef = useRef(null);
   const enterButtonRef = useRef(null);
 
   // Handle responsive background image
@@ -16,7 +15,6 @@ export default function LandingPage() {
   
     const handleResize = () => {
       setBackgroundImage(window.innerWidth < 460 ? "/background_image_fyr3.jpg" : "/background_image_fyr.jpg");
-      setAddMarginTop(window.innerWidth < 460);
     };
   
     handleResize();
@@ -50,19 +48,17 @@ export default function LandingPage() {
     <>
       {/* Hero Section */}
       <div className="landing-page-container" ref={backgroundRef}>
-        <img className="background-image" src={backgroundImage} alt="Background" 
-          style={{ marginTop: addMarginTop ? "7em" : "0" }}
-        />
+        <img className="background-image" src={backgroundImage} alt="Background" />
         <motion.div
           ref={enterButtonRef}
           className="enter-site-button"
-          onClick={() => window.scrollTo({ top: backgroundRef.current.scrollHeight, behavior: "smooth" })}
+          onClick={() => lineRef.current?.scrollIntoView({ behavior: "smooth", block: "start" })}
         >
           ↓ Artists ↓
         </motion.div>
       </div>
 
-      <hr style={{ marginTop: addMarginTop ? "20em" : "5em" }} />
+      <hr className="performance-line-1" ref={lineRef}/>
       {/* Artists Section */}
       <ArtistGrid setExpandedArtist={setExpandedArtist} />
     </>
