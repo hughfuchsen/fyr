f15d9f9fc75c4075119a3dc7d6d634f2609f9cf7
hughfuchsen <hughfuchsen@gmail.com>
2025-04-03 14:44:38 +1100

    p

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Footer = () => {
+
+    return (
+        
+        <div className="footer-container">
+           <p className="footer-element">FLIPPIN YEAH RECORDS</p>
+           <p className="footer-element">P.O. BOX 821 GYMPIE, QLD 4570</p>
+           <p className="footer-element">yes</p>
+        </div>
+    );
+};
+
+export default Footer;
