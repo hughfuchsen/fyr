cb98ddbda60566d2f3ed8814d2847e704df9465a
hughfuchsen <hughfuchsen@gmail.com>
2025-07-15 18:24:48 +1000

    .

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,6 +7,8 @@ const Footer = () => {
         <div className="footer-container">
            <a href="https://flippinyeahindustries.bigcartel.com/products" className="footer-element" target="_blank" rel="noopener noreferrer">MERCH STORE</a>
            <hr className="footer-divider" />
+           <a href="https://8e2543ee.sibforms.com/serve/MUIFANkODF8y017oI5UGp3-BUFM-dHAJ2pGu-delB5mkbVbsMaZHimEf2Nl46meJGMa7vXpOedAVsNTDJOZpwCCfxjHprSQcEbNbgkMR9AWghXBeJtsOK_8MAqhcbeizyjVbW4rsVKXsdo_eS4kUF1BwzqgU7eghp7NbQ8AQBc9q7Ix7sM1h2S20CzMVaVFn6HEU1JiYo4FSb_h8" className="footer-element" target="_blank" rel="noopener noreferrer">MAILING LIST</a>
+           <hr className="footer-divider" />
            <p style={{color:"black"}} className="footer-element">P.O. BOX 821 GYMPIE, QLD 4570</p>
         </div>
     );
