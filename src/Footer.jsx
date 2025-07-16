062f9e3351997042f3825ecb36eb2a7da3c3b498
hughfuchsen <hughfuchsen@gmail.com>
2025-07-16 10:33:59 +1000

    .

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="footer-container">
            <a href="https://flippinyeahindustries.bigcartel.com/products" className="footer-element" target="_blank" rel="noopener noreferrer">MERCH STORE</a>
            <hr className="footer-divider" />
-           <a href="https://8e2543ee.sibforms.com/serve/MUIFANkODF8y017oI5UGp3-BUFM-dHAJ2pGu-delB5mkbVbsMaZHimEf2Nl46meJGMa7vXpOedAVsNTDJOZpwCCfxjHprSQcEbNbgkMR9AWghXBeJtsOK_8MAqhcbeizyjVbW4rsVKXsdo_eS4kUF1BwzqgU7eghp7NbQ8AQBc9q7Ix7sM1h2S20CzMVaVFn6HEU1JiYo4FSb_h8" className="footer-element" target="_blank" rel="noopener noreferrer">MAILING LIST</a>
+           <a href="https://8e2543ee.sibforms.com/serve/MUIFAKMA7B-2jYf35TEf85p5atS-Hi1A6Kk2RGntAap7SmeZ5iieNbD0RY7Af8mWdGRyqIh2Nw2qHa9v25I421hiKi-yCn8tfbhvntqNcxfW-r3-ivlruyMRvGuACIPNeL_WFaVAY7MBAcB8_D-cxzKG1J19VlkXSV9DxOhTvQha_b-D3IqDJL6UuXGJNjmOlRN9zdsffbLrCJHl" className="footer-element" target="_blank" rel="noopener noreferrer">MAILING LIST</a>
            <hr className="footer-divider" />
            <p style={{color:"black"}} className="footer-element">P.O. BOX 821 GYMPIE, QLD 4570</p>
         </div>
