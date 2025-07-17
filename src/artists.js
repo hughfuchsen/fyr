1312978e9c06a321ebc9a8081129d648db58d5c2
hughfuchsen <hughfuchsen@gmail.com>
2025-07-18 06:29:10 +1000

    uploadedImages

--- src/artists.js
+++ src/artists.js
@@ -1,7 +1,7 @@
 const artists = [
   {
     name: "Darren Hanlon",
-    images: ["/darren.jpg"],
+    images: ["/darren.jpg", "/darrenHanlon2.jpg", "/darrenHanlon3.jpg", "/darrenHanlon4.jpg", "/darrenHanlon5.jpg"],
     bio: `The Darren Hanlon universe can be summed up in two words: human connection. He has built a self-sustaining career via his catalog of songs alone. His shows are built around his narrative songwriting and storytelling, and his intimate connection with the audience.
     \n\n
     He has recorded many albums and spent years traveling the far-flung corners of the world on his own terms. He doesn’t chase trends. In fact, he seems delightfully allergic to them. He still tours solo or with a rotating cast of musical friends, playing in churches, lounge rooms, and tiny halls where every lyric lands like it was written just for you. And while most artists slow down over time, Hanlon ages like your favourite pair of jeans—worn in all the right places, getting more comfortable, honest, and quietly brilliant with every year.`,
@@ -19,7 +19,7 @@ const artists = [
   },
   {
     name: "Kankawa Nagarra",
-    images: ["/kankawa.jpg"],
+    images: ["/kankawa.jpg", "/kankawaNagarra2.jpg", "/kankawaNagarra3.jpg"],
     bio: `Kankawa Nagarra is a Walmatjarri Elder, teacher and mentor, human rights advocate, and a passionate environmental activist. Born in the traditional lands of the Gooniyandi and Walmatjarri peoples of north western Australia Her journey—from mission life to AMP winner—is a powerful testament to resilience, creativity, and cultural stewardship.
     \n\n
     Kankawa spent her childhood listening to the tribal songs at cultural ceremonies, and when she was taken from her family to the mission she was taught hymns and Gospel songs with the choir. On the pastoral lease where she was sent to work Country Music was everywhere and she heard Rock and Roll for the first time on the station gramophone.
@@ -41,28 +41,32 @@ const artists = [
   },
   {
     name: "Jack Davies",
-    images: ["/jackD.jpg"],
+    images: ["/jackD.jpg", "/jackD2.jpg", "/jackDavies3.jpg"],
     bio: `Jack Davies is a celebrated West Australian songwriter, well known for his slick finger picking and rambling poetry. Often performing with his group "Jack Davies and The Bush Chooks", the young singer has garnered a dedicated national audience.`,
     website: "https://www.jackdaviesandthebushchooks.com",
     productLink: "https://flippinyeahindustries.bigcartel.com/artist/jack-davies"
   },
-  // {
-  //   name: "Roger Knox",
-  //   images: ["/artist_image_wireframe.jpg"],
-  //   bio: "bio",
-  //   website: "https://rogerknoxmusic.com",
-  //   productLink: "https://flippinyeahindustries.bigcartel.com/artist/david-dondero"
-  // },
+  {
+    name: "Roger Knox",
+    images: ["/Roger1.jpg", "/Roger0.jpg", "/Roger2.jpg", "/Roger3.jpg", "/Roger5.jpg"],
+    bio: `Roger Knox is a legendary Aboriginal Australian country singer-songwriter, affectionately known as the “Black Elvis” and dubbed the “Koori King of Country.” Born in 1948 into the Gomeroi nation at Toomelah Mission near Moree, NSW, he grew from a gospel-rooted childhood into a transformative cultural icon in country music.
+    \n\n
+    Survived two devastating plane crashes in 1980–81 that nearly claimed his life; his recovery marked a spiritual rebirth and led to the formation of the Euraba Band (named after a healing bush and place)
+    \n\n
+    "Buluunarbi and The Old North Star" is this Roger’s first studio album in over 12 years and it’s also the first time in his 50 year career that he’s turned his hand to songwriting. Up until now Roger has been known as a master song interpreter but this album offers a collection of songs that talk of his own history, feelings and experience. For the past few years Roger has been working closely with songwriters Toby Martin, Jon Langford and Jason Walker to craft these stories that he’s carried with him his whole life. This album is very special in that it marks a new phase in the musical journey of one of Australia’s most important artists.`,
+    website: "https://rogerknoxmusic.com",
+    productLink: "https://flippinyeahindustries.bigcartel.com/artist/david-dondero"
+  },
   {
     name: "Jonathan Richman",
-    images: ["/jonathan.jpg"],
-    bio: ``,
+    images: ["/jonathan.jpg", "jonathanRichman2.jpg"],
+    bio: `Flippin Yeah Records are honoured to have Jonathan Richman on the label as his music (and ways of thinking about music) have influenced many of the other artists on the label. Much of his long history has been written about many places elsewhere. Why is he releasing music on this little Australian label? When Darren Hanlon was visiting him in his little town in California he expressed that he’d like his music in Australia to be easier to find. He also found Hanlon’s way of operating appealed to him. We hope to release more of his new music in the future.`,
     website: "null",
     productLink: "https://flippinyeahindustries.bigcartel.com/artist/jonathan-richman"
   },
   {
     name: "Ruth Parker",
-    images: ["/ruthParker.jpg","/darren.jpg"],
+    images: ["/ruthParker.jpg"],
     bio: `Ruth Parker is a gifted folk musician whose journey into songwriting began later in life, proving that creativity is age-independent. Starting her musical career while living in an isolated fishing village in Western Australia, Ruth began writing the first songs that would eventually lead her to creating a full length record. It wasn’t until her move to Melbourne that she truly found her voice, crafting stories and melodies that resonate deeply with the human experience.\n\n 
     In 2019, in her early 60s, Ruth released her debut album Cul De Sac. This gave listeners a first taste of her music, both deeply confessional and evocative. Ruth’s latest album, Otherwise Occupied, promises to be a significant contribution to the Australian folk music scene. Written during the challenging months of Melbourne’s lockdowns, the album is an introspective journey that delves into themes of loss and the solace she found in songwriting.`,
     website: "null",
@@ -70,14 +74,16 @@ const artists = [
   },
   {
     name: "Jerrah Patston",
-    images: ["/jerrahPatston.jpg"],
-    bio: ``,
+    images: ["/jerrahPatston.jpg", "/jerrahPatston2.jpg", "/jerrahPatston3.jpg"],
+    bio: `Jerrah Patston is a prolific singer/songwriter with a distinct sound which combines elements of country and new folk with an ear for an anthemic pop chorus. Patston is gifted with a deft ability to channel ordinary and extraordinary observations about the world around him into catchy songs that will get stuck in your head.
+    \n\n
+    His upcoming album is Abandoned Cricket Games, recorded over two days at Frying Pan Studios in Berriedale, Tasmania, using the ex-Abbey Road REDD.17 recording console documents minor catastrophes, in the classic tradition of folk-rock songs about disaster and disappointment.`,
     website: "null",
     productLink: "https://flippinyeahindustries.bigcartel.com/artist/jerrah-patston"
   },
   {
     name: "Kangaroos With Machine Guns",
-    images: ["/kwmg.jpg"],
+    images: ["/kwmg.jpg", "/kwmg2.jpg"],
     bio: `Kangaroos with machine guns is a band that does not rehearse or write set lists. They believe that music is the union between the personal internal world and the mystical. They are driven by the glittering glistening moment of creation. Many of the songs on this new record are performed by the band for the very first time. They are not perfect but there is a freshness and vibrancy akin to a lightning strike, or a first or last breath.
     \n\n
     They are a bi-coastal set up, sometimes in Boorloo, sometimes all over Australia. Fronted by celestial bagman Archer, with Luke Dux on his electrafried guitar , Ryan Dux on the Bull Fiddle and Todd Pickett bangin stuff withim heart.
@@ -90,7 +96,7 @@ const artists = [
   },
   {
     name: "The Grand Magoozi",
-    images: ["/susie3.JPG"],
+    images: ["/susie3.jpg", "/theGrandMagoozi2.jpg", "/theGrandMagoozi3.jpg", "/theGrandMagoozi4.jpg"],
     bio: `Like a seductive David Lynch chanteuse, The Grand Magoozi occupies the stage in a dreamy twilight world. Susie Scurry’s smoky vocals deliver lament and subtle wit in equal measure, pondering a modern world gone awry.\n\n
     Drawing on formative influences that take in Appalachian string-band music and the canon of Roy Orbison, Scurry calls from a time of burnished musical purity. Infused with both dark space and languor, her songs also honour the likes of Patsy Cline, Karen Dalton and Billy Holiday. The Grand Magoozi is a voice in the lonesome night demanding an earnest and endearing ear, imploring us to see through the haze and make simpler sense of today’s world.`,
     website: "null",
