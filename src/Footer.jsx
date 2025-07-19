a13104c1e24ecd4d21c9e089b34c457f00171371
hughfuchsen <hughfuchsen@gmail.com>
2025-07-19 15:08:38 +1000

    .

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         
         <div className="footer-container">
-           <a href="https://flippinyeahindustries.bigcartel.com/products" className="footer-element" target="_blank" rel="noopener noreferrer">MERCH STORE</a>
+           <a href="https://flippinyeahindustries.bigcartel.com/products" className="footer-element" rel="noopener noreferrer">MERCH STORE</a>
            <hr className="footer-divider" />
            <a href="https://8e2543ee.sibforms.com/serve/MUIFAL_MSS7kxEtWIeXdMTAGmKRywgR1coojPTuSYyQb3p3jwQJy-CBOVYCvZySvkWx6KZiUda5fs0UtHHNA7OpO8G7HLIzuofVB-d9l8G7RHaUk5vY6JfeJixa3VkW0QX_gvwYvmTNOGE-hC6RXhdTUp5dn9T7XuMvPrOzySwIGlHSZLmVlVm3jCLJUKPzwkaNmEj5h5Ne757Sm" className="footer-element" target="_blank" rel="noopener noreferrer">MAILING LIST</a>
            <hr className="footer-divider" />
